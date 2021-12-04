5c1920162778145e694d47b7900284e8ced5eb12
Valerii Bihun <valeriibihun.co@gmail.com>
2021-12-04 11:23:39 -0700

    seeds function created
    THANKS SAM

--- models/index.js
+++ models/index.js
@@ -2,23 +2,23 @@ const User = require('./User');
 const Game = require('./Game');
 const Comment = require('./Comment');
 
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
+// User.hasMany(Comment, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
 
-Comment.belongsTo(Use, {
-  foreignKey: 'user_id'
-});
+// Comment.belongsTo(Use, {
+//   foreignKey: 'user_id'
+// });
 
-Game.hasMany(Comment, {
-    foreignKey: 'game_id',
-    onDelete: 'CASCADE'
-});
+// Game.hasMany(Comment, {
+//     foreignKey: 'game_id',
+//     onDelete: 'CASCADE'
+// });
   
-  Comment.belongsTo(Game, {
-    foreignKey: 'game_id'
-});
+//   Comment.belongsTo(Game, {
+//     foreignKey: 'game_id'
+// });
   
 
 module.exports = { User, Game, Comment };
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,21 +1,25 @@
-const sequelize = require('../config/connection');
-const { User, Game } = require('../models');
+const sequelize = require("../config/connection");
+const { User, Game } = require("../models");
 
-const userData = require('./userData.json');
-const gameData = require('./gameData.json');
+const userData = require("./userData.json");
+const gameData = require("./gameData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
+  // const users = await User.bulkCreate(userData, {
+  //   individualHooks: true,
+  //   returning: true,
+  // });
 
   for (const game of gameData) {
     await Game.create({
-      ...game,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+     status: game.status,
+     home_name: game.home.name,
+     home_alias: game.home.alias,
+     away_name: game.away.name,
+     away_alias: game.away.alias,
+     date_played: game.scheduled
     });
   }
 
