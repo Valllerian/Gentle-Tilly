648cd210e61bdbea2282b5f1812b76c89c9f0fe3
Valerii Bihun <valeriibihun.co@gmail.com>
2021-12-06 20:55:55 -0700

    HADNLEBARS WORKS

--- controllers/mainRoutes.js
+++ controllers/mainRoutes.js
@@ -1,40 +1,34 @@
 const router = require("express").Router();
-const { User, Game, Comment, Team } = require("./../models");
-const withAuth = require('../utils/auth');
-
+const { User, Game, Comment, Team } = require("../models");
+const withAuth = require("../utils/auth");
 
 // Prevent non logged in users from viewing the homepage
-router.get('/', withAuth, async (req, res) => {
+// router.get('/', withAuth, async (req, res) => {
+router.get("/",  async (req, res) => {
   try {
-    const gamesData = await Game.findAll({
-      include: [
-        {
-          model: Team,
-          attributes: ['name', 'alias'],
-        },
-      ],
-    });
+    console.log("router.get")
+    const gamesData = await Game.findAll();
 
     const games = gamesData.map((schedule) => schedule.get({ plain: true }));
-
-    res.render('homepage', {
-      games,
-      // Pass the logged in flag to the template
-      logged_in: req.session.logged_in,
+    res.render("homepage", {
+      games
     });
+    // res.render('main', {
+    //   games
+    // });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
-  // If a session exists, redirect the request to the homepage
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
+// router.get('/login', (req, res) => {
+//   // If a session exists, redirect the request to the homepage
+//   if (req.session.logged_in) {
+//     res.redirect('/');
+//     return;
+//   }
 
-  res.render('login');
-});
+//   res.render('login');
+// });
 
 module.exports = router;
