9fcb4db9b92d31ec7be90ecdef6253787dcfcd8c
Valerii Bihun <valeriibihun.co@gmail.com>
2021-12-13 07:32:55 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,22 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
-//....
+let sequelize;
+
+if (process.env.JAWSDB_URL) { 
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else { //else run it locally
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+// working on heroku
 module.exports = sequelize;
